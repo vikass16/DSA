4a8dcf2135651943523d6df8d98a2f008a907238
Vikas kumar <149498065+vikass16@users.noreply.github.com>
2026-08-11 23:33:36 +0530

    Time: 5 ms (98.64%) | Memory: 44 MB (42.45%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (map1[c1] != map2[c2]) {
+                return false;
+            }
+
+            map1[c1] = i + 1;
+            map2[c2] = i + 1;
+        }
+
+        return true;
+    }
+}
