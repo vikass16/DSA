0fdcbc261ca7d6775d314c6e7193e1e430536c2d
Vikas kumar <149498065+vikass16@users.noreply.github.com>
2026-09-11 01:01:43 +0530

    Time: 5 ms (98.69%) | Memory: 43.7 MB (74.02%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -2,20 +2,18 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
 
         if(s.length() != t.length()) return false;
-        
+
         int[] freq1 = new int[128];
         int[] freq2 = new int[128];
 
         for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            char ch1 = t.charAt(i);
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
 
-            if(freq1[ch] != freq2[ch1]){
-                return false;
-            }
+            if(freq1[ch1] != freq2[ch2]) return false;
 
-            freq1[ch] = i+1;
-            freq2[ch1] = i+1;
+            freq1[ch1] = i+1;
+            freq2[ch2] = i+1;
         }
         return true;
     }
